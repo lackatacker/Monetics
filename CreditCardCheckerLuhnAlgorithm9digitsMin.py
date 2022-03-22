7e52c07bfb6b79e31b24c00be1221a39150170a2
lackatacker <80588946+lackatacker@users.noreply.github.com>
2022-03-22 11:00:23 +0100

    Update CreditCardCheckerLuhnAlgorithm9digitsMin.py

--- CreditCardCheckerLuhnAlgorithm9digitsMin.py
+++ CreditCardCheckerLuhnAlgorithm9digitsMin.py
@@ -1,23 +1,21 @@
 a=input()
 sum=0
-i=len(a)
+i=len(a)-1
 count=0
 if(i<9):
     print('Too short to be valid!')
 else:
-    for k in a:
-        k=int(k)
-    check=int(a[i-1])
-    i-=2
+    a=[int(x) for x in a]
+    check=a[i]
+    i-=1
     a=a[i::-1]
     while (count+1)<=i:
-        u=int(a[count])*2
+        u=a[count]*2
         if u>9:u=1+u%10
-        new=u+int(a[count+1])
-        sum+=u+int(a[count+1])
+        sum+=u+a[count+1]
         count+=2
     if(count==i):
-        u=int(a[i])*2
+        u=a[i]*2
     if u>9:u=1+u%10
     sum+=u
     sum*=9
@@ -25,4 +23,3 @@ else:
         print("Valid Card!")
     else:
         print("Invalid Card!")
-
